1386128f391f871230ab601df1ea5bed9318df32
Madhan <madhanreddy400@gmail.com>
2023-11-09 15:00:13 +0530

    firstCommit

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,7 @@
+const HomePage = ()=><div>
+    <h1>This is from HomePage</h1>
+    <p>This is a sample function from the home page</p>
+    <p>Wait for the form to be completed</p>
+</div>
+
+export default HomePage
