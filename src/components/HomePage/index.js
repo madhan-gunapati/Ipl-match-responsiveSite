5cb20b2264aa3651974f5467bbd5356bb935db8f
madhan-gunapati <127531730+madhan-gunapati@users.noreply.github.com>
2023-11-11 13:12:30 +0530

    Deployment check 2
    This is a second check

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,6 +1,7 @@
 const HomePage = ()=><div>
     <h1 class='text-center font-weight-bold text-info'>Super Over League</h1>
      <p>Editing this file to check changes in vercel </p>
+    <p>Deployment check 2 </p>
     <div class='d-flex flex-row justify-content-center container'>
         <div class='row'>
         <img class=' col-xs-12 col-md-6' src='https://assets.ccbp.in/frontend/react-js/rcb-img.png' alt="RCB-image" />
