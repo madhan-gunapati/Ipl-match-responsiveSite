19e0173e4604c31e039e1a8549cfc227338ad3e8
madhan-gunapati <127531730+madhan-gunapati@users.noreply.github.com>
2023-11-11 13:09:37 +0530

    vercel deployment check
    checking vercel deployment after adding a sample p element

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,5 +1,6 @@
 const HomePage = ()=><div>
     <h1 class='text-center font-weight-bold text-info'>Super Over League</h1>
+     <p>Editing this file to check changes in vercel </p>
     <div class='d-flex flex-row justify-content-center container'>
         <div class='row'>
         <img class=' col-xs-12 col-md-6' src='https://assets.ccbp.in/frontend/react-js/rcb-img.png' alt="RCB-image" />
@@ -7,7 +8,7 @@ const HomePage = ()=><div>
     
         </div>
     </div>
-    <p>Editing this file to check changes in vercel </P>
+   
 </div>
 
 export default HomePage
